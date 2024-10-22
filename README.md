90eb4ea72f14e5872e0a9c72fd2edd47e2e6c256
Patrick Hughes <patrick56hughes@gmail.com>
2024-10-22 10:19:38 -0400

    Created the Player State Machine

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
+The Read me File will be used as a log
+
+-----Branch: playerStateMachine-----
+
+switch statments dont exist in Godot, instead they have mathc, which function similarly
+
+
+ - How to wall jump:
+    Trying to move into the wall and falling, then press jump
+    Need to be sliding to wall jump
+        - create a state where your sliding, then if sliding and jump allow wall jump
+            - ***Create a State Machine? - Somethign to keep track of which state the character is in
+        Sliding: 
+            Criterial: 
+                - Not touching ground
+                - velocity.y is negative
+                - input on side of the wall is being pressed
+                    - direction is not 0
+            Effect:
+                - Gravity is halfed when sliding / gravity is lowered
+            Sprite: 
+                - Will need one animation, preferably that has motion, and is moer than one slide
+                - Add dust particles usign the particle node for style points
+            Sound: 
+                - When sliding, play wall sliding noise
+                - To play sliding noise seamlessly, have the noise auto play, but when in sliding state turn volume up, and when not mute
+                - For wall jump, use button tick noise and jump noise at same time
+
+ - Have a bool that will keep track of my jumping
+    - Sets to True at start, then any time you jump it goes false, and doesnt become true again until you touch the ground/land
+    - This variable will be tied into jumping, and you wont be allowed to jump until its true again.
+
+
 Can I add a read me through GitHub and then pull it?
+ - Yes
--- scripts/player.gd
+++ scripts/player.gd
@@ -8,8 +8,11 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var direction=0
 var isDead = false
-
+# for the State Machine - the are capitlas because they are treated as constants
+enum state {DEAD, IDLE, RUNNING, JUMPING, SLIDING, FALLING}
+var currentState
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -17,19 +20,64 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func hasDied():
 	isDead = true
 
+
 func _ready():
-	isDead = false
+	pass
+
 
 # Gets called every frame
 func _process(delta):
-	pass
+	# State Machine - I dont know if this is actualy what a state machine is, but
+	# my State Machine is used to keep track of what state the player is in. I put 
+	# this here instead of in the _physics_process because I want to leave that for 
+	# physics, and this should be being called more frequently anyways, so this will 
+	# work
+	if(isDead):
+		currentState = state.DEAD
+	elif(is_on_floor()):
+		if(direction==0):
+			currentState = state.IDLE
+		else:
+			currentState = state.RUNNING
+	else:
+		if(Input.is_action_just_pressed("jump")):
+			currentState = state.JUMPING
+		if(velocity.y>0):
+			currentState = state.FALLING
+			if(is_on_wall()):
+				currentState = state.SLIDING
+	#print(currentState)
+
 
 # Gets called consintently, so physics works as it should (by default 60 tiems per second)
 func _physics_process(delta):
+	
+	#print("Current:" + str(currentState))
+	match currentState:
+		state.DEAD:
+			print("State: Dead")
+		state.IDLE:
+			print("State: IDLE")
+		state.RUNNING:
+			print("State: RUNNING")
+		state.JUMPING:
+			print("State: JUMPING")
+		state.SLIDING:
+			print("State: SLIDING")
+		state.FALLING:
+			print("State: FALLING")
+	
+	
 	# Add Gravity
 	if(not is_on_floor()):
 		velocity.y += gravity * delta
-
+	
+	# Handles SLiding
+	if((not is_on_floor()) and is_on_wall() and velocity.y>0 and direction!=0):
+		# Sets max fall speed when sliding
+		if(velocity.y>50):
+			velocity.y=50
+	
 	# Handles Jumps
 	if(Input.is_action_just_pressed("jump") and is_on_floor()):
 		jump.play()
@@ -37,7 +85,7 @@ func _physics_process(delta):
 
 	# get_axis("Negative Value", "Positive Value")
 	# Gets the input direction
-	var direction = Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")
 	
 	# If the player is NOT dead
 	if(!isDead):
