f7fc9bb3e6bf7d56bff6df2e92a738d32a664936
Patrick <patrick56hughes@gmail.com>
2024-11-08 22:50:19 -0500

    Commiting because I forgot to make a branch

---
+++ scripts/home_page.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var folder_ui = %Folder_UI
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+	
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
+
+
