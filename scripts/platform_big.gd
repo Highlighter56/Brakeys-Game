3ddfe3239d15c3f00f7d17bdcfdb0906ce6e911a
Patrick <patrick56hughes@gmail.com>
2024-12-30 18:52:11 -0500

    Progress on level 3

--- scripts/platform_big.gd
+++ scripts/platform_big.gd
@@ -17,6 +17,16 @@ func _on_animation_player_animation_finished(anim_name):
 	animationSequence+=1
 	match animationSequence:
 		1:
-			animation_player.play("move_right")
+			animation_player.play("2_second_wait")
 		2:
+			animation_player.play("move_right")
+		3:
+			animation_player.play("7_second_wait")
+		4:
 			animation_player.play("move_up")
+		5:
+			animation_player.play("move_through_clouds")
+		6:
+			animation_player.play("5_second_wait")
+		7:
+			animation_player.play("sink")
